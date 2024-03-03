e6a22eabd6bba18df3f67a7f35438ae32cc515d9
jocehyj <99947994+JoceHYJ@users.noreply.github.com>
2024-03-03 11:11:16 +0800

    UPDATE STATIC_MATCHING_ROUTER&TEST(TDD_1) router: 简单的路由器实现，用于构建和管理路由树，以便将HTTP请求映射到相应的处理函数上。 router_test: 测试路由树，通过构造路由树并进行断言来验证路由树的正确性。通过自定义equal方法来实现对路由器和节点的比较。

--- web/router.go
+++ web/router.go
@@ -1,5 +1,7 @@
 package web
 
+import "strings"
+
 // 全静态匹配
 
 // router
@@ -25,8 +27,46 @@ func newRouter() *router {
 	}
 }
 
-func (r *router) AddRoute(method, path string, handler HandleFunc) {
+// AddRoute 添加路由的方法
+func (r *router) AddRoute(method, path string, handleFunc HandleFunc) {
+	// 找到对应的树
+	root, ok := r.trees[method]
+	if !ok {
+		// 没有根节点则创建
+		root = &node{
+			path: "/",
+		}
+		r.trees[method] = root
+	}
+	// 切割 path
+	// /user/home 会被切割成 ["", "user", "home"]三段
+	// 第一段是空的，需要去掉第一段
+	path = path[1:]
+	segs := strings.Split(path, "/")
+	for _, seg := range segs {
+		// 递归寻找位置 --> children
+		// 如果中途有节点不存在则创建
+		children := root.childOrCreate(seg)
+		root = children
+	}
+	// 把 handler 挂载到 root 上(赋值)
+	root.handler = handleFunc
+}
 
+// childOrCreate 用于查找或创建节点的子节点
+func (n *node) childOrCreate(seg string) *node {
+	if n.children == nil {
+		n.children = map[string]*node{}
+	}
+	res, ok := n.children[seg]
+	if !ok {
+		// 如果子节点不存在，则新建一个
+		res = &node{
+			path: seg,
+		}
+		n.children[seg] = res
+	}
+	return res
 }
 
 type node struct {
---
+++ web/router_test.go
@@ -0,0 +1,106 @@
+package web
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+// TestRouter_AddRoute 测试路由树
+func TestRouter_AddRoute(t *testing.T) {
+	// 1.构造路由树
+	// 2.验证路由树
+	testRoutes := []struct {
+		method  string
+		path    string
+		handler HandleFunc
+	}{
+		{ // 测试 GET 方法
+			method: http.MethodGet,
+			path:   "/user/home",
+		},
+	}
+
+	var mockHandler HandleFunc = func(ctx Context) {}
+	r := newRouter()
+	for _, route := range testRoutes {
+		r.AddRoute(route.method, route.path, mockHandler)
+	}
+
+	// 3.断言两者相等
+	wantRouter := &router{
+		trees: map[string]*node{
+			http.MethodGet: &node{
+				path: "/",
+				children: map[string]*node{
+					"user": &node{
+						path: "user",
+						children: map[string]*node{
+							"home": &node{
+								path:     "home",
+								children: map[string]*node{},
+								handler:  mockHandler,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// 不能直接断言, 因为 HandleFunc 不是可比较的类型
+	// assert.Equal(t, wantRouter, r)
+
+	msg, ok := wantRouter.equal(r)
+	assert.True(t, ok, msg)
+}
+
+// equal 方法: 断言
+// string 返回错误信息 --> 排查问题
+// bool 返回是否相等
+func (r *router) equal(y *router) (string, bool) {
+	for k, v := range r.trees {
+		dst, ok := y.trees[k]
+		if !ok {
+			return fmt.Sprintf("找不到对应的 http method"), false
+		}
+		// v, dst 要相等
+		msg, equal := v.equal(dst)
+		if !equal {
+			return msg, false
+		}
+	}
+	return "", true
+}
+
+func (n *node) equal(y *node) (string, bool) {
+	if n.path != y.path {
+		return fmt.Sprintf("节点路径不匹配"), false
+	}
+	if len(n.children) != len(y.children) {
+		return fmt.Sprintf("子节点数量不匹配"), false
+	}
+
+	// 比较 handler --> 利用反射
+	nHandler := reflect.ValueOf(n.handler)
+	yHandler := reflect.ValueOf(y.handler)
+	if nHandler != yHandler {
+		return fmt.Sprintf("Handler 不匹配"), false
+	}
+
+	// 递归比较子节点
+	for path, c := range n.children {
+		dst, ok := y.children[path]
+		// TODO: ok? !ok?
+		if !ok {
+			return fmt.Sprintf("子节点 %s 不存在", path), false
+		}
+		msg, ok := c.equal(dst)
+		if !ok {
+			return msg, false
+		}
+	}
+	return "匹配成功", true
+}
