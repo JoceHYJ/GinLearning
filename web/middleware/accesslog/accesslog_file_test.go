fbc93f1cbea0df1f35ca0a2f438e277e42c3de52
jocehyj <99947994+JoceHYJ@users.noreply.github.com>
2024-03-17 17:41:29 +0800

    UPDATE ACCESSLOG_FILE 1.新增 NewBulider 方法 2.新增单元测试创建 builder 实例，并生成日志文件进行记录

---
+++ web/middleware/accesslog/accesslog_file_test.go
@@ -0,0 +1,33 @@
+package accesslog
+
+import (
+	"GinLearning/web"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestMiddlewareBuilder_Log_file(t *testing.T) {
+	// 创建一个Builder对象
+	builder := NewBuilder()
+
+	// log 文件路径
+	logFile, err := os.Create("access.log")
+	if err != nil {
+		t.Fatalf("Failed to create log file: %v", err)
+	}
+
+	defer logFile.Close()
+
+	mdl := builder.LogFunc(func(log string) {
+		logFile.WriteString(fmt.Sprintf("%s: %s \n", time.Now().Format(time.RFC3339), log))
+	}).Build()
+
+	server := web.NewHTTPServer(web.ServerWithMiddleware(mdl))
+	server.Get("/hello", func(ctx *web.Context) {
+		ctx.Resp.Write([]byte("Hello log!"))
+	})
+
+	server.Start(":8081")
+}
